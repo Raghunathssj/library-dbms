5913f70c25afb7c3838c8a7427a2e5c0fe71f0a2
yogiraj_tambake <ytambake@gmail.com>
2018-03-20 09:55:03 +0530

    added a script for adding datapath

--- initialize_data.sql
+++ initialize_data.sql
@@ -1,5 +1,5 @@
 set search_path to step_library;
 
-COPY users from '/users/yogiras/Projects/library-dbms/data/users_data.csv' with delimiter ',';
-COPY book_group from '/users/yogiras/Projects/library-dbms/data/book_group_data.csv' with delimiter ',';
-COPY books from '/users/yogiras/Projects/library-dbms/data/book_data.csv' with delimiter ',';
+COPY users from "${1}/library-dbms/data/users_data.csv" with delimiter ',';
+COPY book_group from '${1}/library-dbms/data/book_group_data.csv' with delimiter ',';
+COPY books from '${1}/library-dbms/data/book_data.csv' with delimiter ',';
